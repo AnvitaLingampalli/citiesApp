93e3b7eb6e1ef811997c26226a2b7ef5d2dddb6e
AnvitaLingampalli <lingampalligunny@gmail.com>
2023-03-04 01:20:24 -0700

    Initial Commit

---
+++ citiesTableView/citiesTableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  citiesTableViewApp.swift
+//  citiesTableView
+//
+//  Created by Anvita Lingampalli on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct citiesTableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
